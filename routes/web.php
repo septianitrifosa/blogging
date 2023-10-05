40b38ae0bf2987ca4bd2bfcc1eb2a3ea1b9941fd
septiani TD <bhogaster@gmail.com>
2023-10-05 18:14:57 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
@@ -29,4 +30,4 @@ Route::get('/about', [AboutController::class, 'index']);
 Route::get('/contact-us', [ContactController::class, 'index'])->name('contact-us.index');
 Route::post('/contact-us', [ContactController::class, 'store'])->name('contact-us.store');
 
-Route::post('/contact-us', [ContactController::class, 'store']);
+Route::resource('articles', ArticleController::class);
