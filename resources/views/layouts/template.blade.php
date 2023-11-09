66f17349252ec36713333726af03db244be3c7cc
septiani TD <bhogaster@gmail.com>
2023-11-09 16:25:36 +0700

    mail trap

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -14,25 +14,45 @@
         <header class="blog-header py-3">
             <div class="row flex-nowrap justify-content-between align-items-center">
                 <div class="col-4 pt-1">
-                    <a class="link-secondary" href="#">Subscribe</a>
+                    <a class="link-secondary" href="{{ route('login') }}">About Us</a>
                 </div>
                 <div class="col-4 text-center">
-                    <a class="blog-header-logo text-dark" href="#">Large</a>
+                    <a class="blog-header-logo text-dark" href="{{ route('login') }}">Blogging</a>
                 </div>
                 <div class="col-4 d-flex justify-content-end align-items-center">
-                    <a class="link-secondary" href="#" aria-label="Search">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
-                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            class="mx-3" role="img" viewBox="0 0 24 24">
-                            <title>Search</title>
-                            <circle cx="10.5" cy="10.5" r="7.5"></circle>
-                            <path d="M21 21l-5.2-5.2"></path>
-                        </svg>
-                    </a>
-                    <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+                    @guest
+                        <a class="p-2 link-secondary" href="{{ route('login') }}">{{ __('Login') }}</a>
+
+                        <a class="p-2 link-secondary" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @else
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    @endguest
                 </div>
             </div>
         </header>
+
+        @auth
+            <div class="nav-scroller py-1 mb-2">
+                <nav class="nav d-flex justify-content-center">
+                    <a class="p-2 link-secondary" href="{{ route('articles.index') }}">Article</a>
+                </nav>
+            </div>
+        @endauth
 
         <div class="nav-scroller py-1 mb-2">
             <nav class="nav d-flex justify-content-between">
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,7 @@ Route::resource('articles', ArticleController::class);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
